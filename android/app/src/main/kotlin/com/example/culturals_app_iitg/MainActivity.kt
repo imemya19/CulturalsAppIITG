d4160e5da90193bc5558e90403821839b9c79c79
imemya19 <somyaagrawal.ime19@gmail.com>
2020-12-10 01:01:01 +0530

    Starting page

---
+++ android/app/src/main/kotlin/com/example/culturals_app_iitg/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.culturals_app_iitg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+Widget topsection = Container(
+  padding: const EdgeInsets.all(50),
+  color: Colors.amber,
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.end,
+    children: [
+      Text(
+        "Skip",
+        style: TextStyle(
+          fontFamily: "Montserrat",
+          fontWeight: FontWeight.w600,
+          color: Color(0xFFB9B9B9),
+        ),
+      ),
+    ],
+  ),
+);
+
+Widget imagesection = Container(
+  padding: const EdgeInsets.all(5),
+  color: Colors.pink,
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Image.asset('../assets/images/Group504.png'),
+    ],
+  ),
+);
+
+Widget textsection = Container(
+  padding: const EdgeInsets.all(3),
+  color: Colors.yellow,
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        'Online Competitions',
+        style: TextStyle(
+          color: Colors.black,
+          fontFamily: "Montserrat",
+          fontWeight: FontWeight.w700,
+          fontSize: 24,
+        ),
+      ),
+      Container(
+        padding: const EdgeInsets.only(top: 10.0, bottom: 20.0),
+        child: Text('Participate and Vote in Cultural \nEvents of IITG',
+            style: TextStyle(
+              color: Color(0XFF959595),
+              fontFamily: "Montserrat",
+              fontWeight: FontWeight.w600,
+              fontSize: 15,
+            ),
+            textAlign: TextAlign.center),
+      ),
+    ],
+  ),
+);
+
+Widget loadersection = Container(
+  padding: const EdgeInsets.all(3),
+  color: Colors.lightGreen,
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Image.asset('../assets/images/Group613.png'),
+    ],
+  ),
+);
+
+Widget arrowsection = Container();
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Culturals IITG',
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              topsection,
+              imagesection,
+              textsection,
+              loadersection,
+              arrowsection,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
