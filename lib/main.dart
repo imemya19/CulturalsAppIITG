838e76ffa4104704fd0813b0a3229d3a16a5b558
imemya19 <somyaagrawal.ime19@gmail.com>
2020-12-18 13:03:19 +0530

    Pages separated

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
+import 'package:culturals_app_iitg/body.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: MyApp(),
-  ));
+  runApp(MyApp());
 }
 
 // =============================
@@ -26,110 +25,19 @@ Widget topsection = Container(
   ),
 );
 
-Widget imagesection = Container(
-  padding: const EdgeInsets.all(30.0),
-  // color: Colors.pink,
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Image(
-        image: AssetImage('assets/images/Group504.png'),
-      ),
-    ],
-  ),
-);
-
-Widget textsection = Container(
-  padding: const EdgeInsets.all(3),
-  // color: Colors.yellow,
-  child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        'Online Competitions',
-        style: TextStyle(
-          color: Colors.black,
-          fontFamily: "Montserrat",
-          fontWeight: FontWeight.w900,
-          fontSize: 24,
-        ),
-      ),
-      Container(
-        padding: const EdgeInsets.only(top: 10.0, bottom: 20.0),
-        child: Text('Participate and Vote in Cultural \nEvents of IITG',
-            style: TextStyle(
-              color: Color(0XFF959595),
-              fontFamily: "Montserrat",
-              fontWeight: FontWeight.w600,
-              fontSize: 15,
-            ),
-            textAlign: TextAlign.center),
-      ),
-    ],
-  ),
-);
-
-Widget loadersection = Container(
-  padding: const EdgeInsets.all(10.0),
-  // color: Colors.lightBlueAccent,
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Image.asset('assets/images/Rectangle380.png'),
-      ),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Image.asset('assets/images/Ellipse100.png'),
-      ),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Image.asset('assets/images/Ellipse100.png'),
-      ),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Image.asset('assets/images/Ellipse100.png'),
-      ),
-    ],
-  ),
-);
-
-Widget arrowsection = Container(
-  padding: const EdgeInsets.all(15.0),
-  // color: Colors.lightGreen,
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Image.asset(
-        'assets/images/Group613.png',
-      ),
-    ],
-  ),
-);
-
-class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Culturals IITG',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Montserrat'),
       home: Scaffold(
         body: SafeArea(
           child: Column(
             children: [
               topsection,
-              imagesection,
-              textsection,
-              loadersection,
-              arrowsection,
+              Body(),
             ],
           ),
         ),
